44ef6a9c05d14496aad71d3f37af00601706376a
marco.parenzan <marco.parenzan@live.it>
2014-10-27 08:20:43 +0100

    First Commit

---
+++ WindowsMakers.Web/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WindowsMakers.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        [Route("Blog/{id}")]
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                return View();
+            else
+                return View(id);
+        }
+    }
+}
---
+++ WindowsMakers.Web/Controllers/TeamController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WindowsMakers.Web.Controllers
+{
+    public class TeamController : Controller
+    {
+        [Route("Team/{id}")]
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                return View();
+            else
+                return View(id);
+        }
+    }
+}
